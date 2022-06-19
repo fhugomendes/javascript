b9faac8fd9ef0425878db8a81841eee37c9dae2e
Hugo Mendes <88986695+fhugomendes@users.noreply.github.com>
2022-06-18 21:40:46 -0300

    novos exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 79.6
+console.log(`Você está à ${vel} km/h.`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre com cuidado')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'França'
+console.log(`Você vive no ${pais}`)
+if(pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é estrangeiro')
+}

f5314d2bfad4a0bf6fddaa0559404f5447385643
Hugo Mendes <88986695+fhugomendes@users.noreply.github.com>
2022-06-28 14:10:36 -0300

    new

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,23 +1,59 @@
-function adicionar(){
-    let nums = []
-    let txtn = window.document.getElementById('txtnum')
-    let n = Number(txtn.value)
-    let res = window.document.getElementById('res')
-    if (txtn.value.length == 0){
-        window.alert('ERRO! Insira um número')
+let num = window.document.getElementById('txtnum')
+let lista = window.document.getElementById('flista')
+let res = window.document.getElementById('res')
+let val = []
+
+function entreNum(n){
+    if (n >= 1 && n <= 100){
+        return true
     }else{
-        if (n > 100 || n < 1){
-            window.alert('ERRO! Insira um número válido')
-        }else {
-            let item = window.document.createElement('option')
-            if (nums.indexOf(n) == -1){
-                item.text = `Número ${n} adicionado`
-                res.appendChild(item)
-                nums.push(n)
-            }else{
-                window.alert('Este número ja foi adicionado anteriormente')
-            }
-            console.log(nums)
-        }
+        return false
     }
+}
+
+function inLista(n, l){
+    if (l.indexOf(n) != -1){
+        return true
+    }else {
+        return false
+    }
+}
+
+function adicionar(){
+    if (num.value.length == 0){
+        window.alert('ERRO! Insira um Número.')
+    }else{
+        if (entreNum(Number(num.value) && !inLista(Number(num.value), val))){
+            let item = window.document.createElement('option')
+            item.text = `Valor ${num.value} adicionado`
+            lista.appendChild(item)
+            val.push(Number(num.value))
+            res.innerHTML = ''
+        }else{
+            if (entreNum(Number(num.value) == false)){
+                window.alert('ERRO! Digite um número entre 1 e 100.')
+            }else{
+                window.alert('ERRO! Esse número já foi adicionado.')
+            }
+        }
+        num.value = ''
+        num.focus()
+    }
+}
+
+function finalizar(){
+    if (val.length == 0){
+        window.alert('ERRO! Adicione os números primeiro.')
+    }else{
+        let soma = 0
+
+        for (let i = 0; i < val.length; i++){
+            soma += val[i]
+        }
+        res.innerHTML = ``
+        res.innerHTML += `Ao todo, temos ${val.length} números cadastrados.`
+        res.innerHTML += `<br>O maior número cadastrado foi: ${Math.max(...val)}`
+        res.innerHTML += `<br>O menor número cadastrado foi: ${Math.min(...val)}`
+        res.innerHTML += `<br>A soma dos números é: ${soma}`
+        res.innerHTML += `<br>A média dos números é: ${soma / val.length}`}
 }
